9808f161bb7281d827e39a4fad737b252038a8cf
Vladimir <v@gkaz.ru>
2024-02-24 09:14:14 +0300

    Changes for demo promisess in classical mode and async/avait mode in cjs and mjs node packages:
    	modified:   project_01_cjs/index.js
    	modified:   project_02_mjs/index.mjs

--- project_01_cjs/index.js
+++ project_01_cjs/index.js
@@ -1,3 +1,77 @@
 const Square = require('./square.js');
 const o = new Square(4);
 console.log(`Square = ${o.area()}`);
+
+
+// async examples (cjs)
+
+// promise
+const p1 = new Promise((resolve, reject) => {
+    console.log('timeout started');
+    setTimeout(() => {
+        console.log('timeout finished');
+        if(Boolean(+((new Date()).getSeconds())%2)){
+            resolve('success');
+        } else {
+            reject('error');
+        }
+    }, 2000);
+});
+
+
+// Not working in cjs mode!
+// try {
+//     const successResult = await p1;
+//     console.log('Async call success: ', successResult);
+// } catch(e) {
+//     console.log('Async call failed: ', e);
+// }
+
+
+// classic working with promise
+// p1.then(state=>{
+//     console.log('then detected', state);
+// }).catch(err=>{
+//     console.log('catch detected', err);
+// }).finally(()=>{
+//     console.log('finally detected');
+// });
+
+
+// working woth promise (async/await) v1
+
+// async await in cjs
+// async function runner(){
+//     try {
+//         const successResult = await p1;
+//         console.log('Async call success: ', successResult);
+//     } catch(e) {
+//         console.log('Async call failed: ', e);
+//     }
+// }
+// runner();
+
+
+
+// working woth promise (async/await) v2
+
+// (async function(){
+//     try {
+//         const successResult = await p1;
+//         console.log('Async call success: ', successResult);
+//     } catch(e) {
+//         console.log('Async call failed: ', e);
+//     }
+// })();
+
+
+// working woth promise (async/await) v3
+
+(async () => {
+    try {
+        const successResult = await p1;
+        console.log('Async call success: ', successResult);
+    } catch(e) {
+        console.log('Async call failed: ', e);
+    }
+})();
--- project_02_mjs/index.mjs
+++ project_02_mjs/index.mjs
@@ -7,3 +7,27 @@ const o2 = new Square2(3);
 console.log(`Square = ${o.area()}`);
 
 console.log(`Square2 = ${o2.area()}`);
+
+
+// async examples (mjs)
+
+// promise
+const p1 = new Promise((resolve, reject) => {
+    console.log('timeout started');
+    setTimeout(() => {
+        console.log('timeout finished');
+        if(Boolean(+((new Date()).getSeconds())%2)){
+            resolve('success');
+        } else {
+            reject('error');
+        }
+    }, 2000);
+});
+
+// Working in mjs mode (but not working in cjs)!
+try {
+    const successResult = await p1;
+    console.log('Async call success: ', successResult);
+} catch(e) {
+    console.log('Async call failed: ', e);
+}
