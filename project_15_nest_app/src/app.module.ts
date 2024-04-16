6372746309b4b151c90772dbc17599f6ef10b8bb
vladimir <v@gkaz.ru>
2024-04-16 22:08:35 +0300

    Project 15: Improve project

--- project_15_nest_app/src/app.module.ts
+++ project_15_nest_app/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { ContactsModule } from './contacts/contacts.module';
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from 'node:path';
+
+const configParams = {
+    isGlobal: true,
+    envFilePath: ['.env'],
+};
+
+const staticDirConfig = {
+    rootPath: join(__dirname, '..', 'client', 'one', 'two', '42'),
+    exclude: ['/api*']
+};
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      ConfigModule.forRoot(configParams),
+      ServeStaticModule.forRoot(staticDirConfig),
+      ContactsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- project_15_nest_app/src/main.ts
+++ project_15_nest_app/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('SERVER_PORT') || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  const url = await app.getUrl();
+
+  console.log(`Server started at ${url}`);
+
 }
 bootstrap();
